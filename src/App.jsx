e7ffc9fb79aaa98da2050bb81cf2ed6adbdfb442
Aadinath1234 <aadeshsatpute375@gmail.com>
2024-04-08 11:04:33 +0530

    react-redux with redux components

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { useContext, useState, useReducer } from 'react';
+import {useDispatch} from 'react-redux'
+import AddTodo from './components/AddTodo/AddTodo';
+import TodoList from './components/TodoList/TodoList';
+import {bindActionCreators} from 'redux';
+import { addTodo, deleteTodo, editTodo, todoFinished} from './actions/todoActions';
+
+
+function App() {
+
+
+
+ const dispatch = useDispatch();
+ const actions = bindActionCreators({ addTodo, deleteTodo, editTodo, todoFinished}, dispatch);
+
+
+  return (
+    <>
+
+      <AddTodo addTodo={actions.addTodo} />
+      <TodoList  deleteTodo={actions.deleteTodo} editTodo={actions.editTodo} todoFinished={actions.todoFinished} />
+    </>
+
+
+  )
+}
+
+
+export default App;
+
+
+
+
---
+++ src/components/ReduxComponents/reduxCompose.mjs
@@ -0,0 +1,21 @@
+import {compose} from 'redux';
+
+function removeSpaces(string){
+      return string.split(" ").join("");
+}
+
+function repeatString(string){
+     return string + string;
+}
+
+function convertToUpper(string){
+      return string.toUpperCase();
+}
+
+const input = "abc def ghi"
+// const output = convertToUpper(repeatString(removeSpaces(input)));
+// console.log(output);
+
+const composedFunction = compose(removeSpaces, repeatString, convertToUpper);
+console.log(composedFunction(input));
+
